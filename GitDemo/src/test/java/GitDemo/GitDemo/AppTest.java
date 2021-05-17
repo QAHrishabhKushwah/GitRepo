8505883e82dfe68ca09754c92437e416eb236d3e
QAHrishabhKushwah <73633760+QAHrishabhKushwah@users.noreply.github.com>
2021-05-17 12:22:02 +0530

    Second commit - changes made

--- GitDemo/src/test/java/GitDemo/GitDemo/AppTest.java
+++ GitDemo/src/test/java/GitDemo/GitDemo/AppTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
+     * 
+     * 
      * Rigorous Test :-)
      */
     @Test
--- GitDemo/src/test/java/GitDemo/GitDemo/ds.java
+++ GitDemo/src/test/java/GitDemo/GitDemo/ds.java
@@ -1,5 +1,8 @@
 package GitDemo.GitDemo;
 
 public class ds {
-
+public ds() {
+	// TODO Auto-generated constructor stubsxdfds
+	
+}
 }
